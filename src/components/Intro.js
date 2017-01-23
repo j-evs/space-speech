94fea9d84191a4dde861712cff78ed01310b3519
j-evs <jan.evseev@gmail.com>
2017-01-23 18:40:29 +0300

    convert component from class to functional, add data for clean routing for deploy

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 
 import '../styles/Intro.less';
 
 import RandomizeButton from './RandomizeButton';
 
-export default class Intro extends Component {
-    render() {
-        return (
-            <div className="intro-wrapper">
-                <div className="intro-container">
-                    <div className="intro">
-                        <h1 className="intro__header">Space Speech</h1>
-                        <div className="intro__promo">
-                            <p className="intro__description">Experience the most dramatic events of XX century from another perspective</p>
-                            <p className="intro__description">Enjoy listening to famous speakers of the past together with a fine selection of ambient-music</p>
-                        </div>
-                        <div className="intro__selection">
-                            <Link className="intro__link" to='/library'>Browse our library of speeches</Link>
-                            <p className="intro__description">OR</p>
-                            <RandomizeButton router={this.props.router}/>
-                        </div>
-                    </div>
+const Intro = (props) => (
+    <div className="intro-wrapper">
+        <div className="intro-container">
+            <div className="intro">
+                <h1 className="intro__header">Space Speech</h1>
+                <div className="intro__promo">
+                    <p className="intro__description">Experience the most dramatic events of XX century from another perspective</p>
+                    <p className="intro__description">Enjoy listening to famous speakers of the past together with a fine selection of ambient-music</p>
+                </div>
+                <div className="intro__selection">
+                    <Link className="intro__link" to='/library'>Browse our library of speeches</Link>
+                    <p className="intro__description">OR</p>
+                    <RandomizeButton router={props.router}/>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    </div>
+);
+
+export default Intro;
